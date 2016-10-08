6c3746877afd25277a56230891c131db9c847fe9
Mark Crossen <markcrossen@studentbody.byu.edu>
2016-10-08 00:34:50 -0600

    cleaner method

--- lib/extendeuclidean.rb
+++ lib/extendeuclidean.rb
@@ -14,12 +14,21 @@ module ExtendedEuclidean
   def self.inv_mult(modulus, num)
     last_remainder = modulus.abs
     remainder = num.abs
-    x, last_x = 0, 1
-    y, last_y = 1, 0
+    x = 0
+    last_x = 1
+    y = 1
+    last_y = 0
     while remainder != 0
-      last_remainder, (quotient, remainder) = remainder, last_remainder.divmod(remainder)
-      x, last_x = last_x - quotient*x, x
-      y, last_y = last_y - quotient*y, y
+      quotient = last_remainder / remainder
+      temp_remainder = remainder
+      remainder = last_remainder % remainder
+      last_remainder = temp_remainder
+      temp_x = x
+      x = last_x - quotient*x
+      last_x = temp_x
+      temp_y = y
+      y = last_y - quotient*y
+      last_y = temp_y
     end
     raise if last_remainder != 1
     (last_x * (modulus < 0 ? -1 : 1)) % num
